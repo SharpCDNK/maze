4d10c06c812ece4a8d9fbb077fec04f0fe6fb08f
sharpcd <nikita.khala.cd2007@gmail.com>
2025-03-27 22:15:53 +0300

    before update

---
+++ build/CMakeFiles/MyExec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExec.dir/main.cpp.o"
+  "CMakeFiles/MyExec.dir/main.cpp.o.d"
+  "MyExec"
+  "MyExec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyExec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hellow maze!\n";
+    std::cout << "Hellow 1\n";
+    std::cout << "Jej\n";
+}
